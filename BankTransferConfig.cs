863ffb61eda738c9a671ab9ce4c3b6110865b6e8
abi948 <abiyulaudza1@gmail.com>
2026-05-02 15:22:14 +0700

    aplikasi bank tranfer menggunakan rantai configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Modul9_103022400038;
+using System.Runtime.CompilerServices;
+
+public class BankTransferConfig
+{
+
+    public Config Config { get; set; }
+    private string filepath;
+
+}
+
+public BankTranserConfig()
+{
+   this.filepath = Path.GetFullPath
+}
+
+public class ReadConfigFile()
+{
+
+}
+
+public class WriteConfigFile()
+{
+    public Config Config { get; set; }
+}
+
+public class Setdefault()
+{
+    public string filepath { get; set; }
+}
---
+++ Confirmation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul9_103022400038
+{
+    public class Confirmation
+    {
+        public string en { get; set; }
+        public string id { get; set; }
+
+        public Confirmation() { }
+        public Confirmation(string en, string id)
+        {
+            this.en = en;
+            this.id = id;
+
+
+        }
+        
+    }
+}
